8d0c872dfbf492f0ce2f8851d3debdd05ef75a85
htehrani98 <h.tehrani1377@gmail.com>
2022-06-25 01:06:07 +0430

    5.Intro To Styled Component

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,34 +1,65 @@
 import React from "react";
 import home1 from "../img/home1.png";
+//Styled
+import styled from "styled-components";
 
 const AboutSection = () => {
   return (
-    <div>
-      <div className="description">
+    <About>
+      <Description>
         <div className="title">
-          <div className="hide">
+          <Hide>
             <h2>We work to make</h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide>
             <h2>
               your <span>dreams</span> come
             </h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide>
             <h2>true.</h2>
-          </div>
+          </Hide>
         </div>
         <p>
           Contact us for any photography or video editing ideas that you have.
-          We have professionals with amazing skills to help achieve it.
+          We have professionals with amazing skills to help you achieve it.
         </p>
         <button>Contact us</button>
-      </div>
-      <div className="image">
+      </Description>
+      <Image>
         <img src={home1} alt="akasam aks migiram" />
-        // and always should import source first and use it just like this
-      </div>
-    </div>
+        {/* and always should import source first and use it just like this */}
+      </Image>
+    </About>
   );
 };
+//Styled Components
+const About = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: gray;
+`;
+const Description = styled.div`
+  flex: 1; // what is this one
+  padding-right: 5rem;
+  h2 {
+    font-weight: lighter;
+  }
+`;
+const Image = styled.div`
+  flex: 1;
+  overflow: hidden;
+  img {
+    width: 100%;
+    height: 80vh; // at 18: 50 my result is deferent
+    object-fit: cover;
+  }
+`;
+const Hide = styled.div`
+  overflow: hidden;
+`;
+
 export default AboutSection;
---
+++ src/components/GlobalStyle.js
@@ -0,0 +1,57 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box; // something that we should write
+}
+body{
+  background: #1b1b1b;
+}
+button{
+  font-weight: bold;
+  font-size: 1.1rem;
+  cursor: pointer;
+  padding: 1rem 2rem; //what this would do
+  border: 3px solid #23d997;
+  background: transparent; //have to check this out to
+  color: white;
+  transition: all 0.5s ease; //have to check this out to
+  &:hover{ //have to check this out to
+    background-color: #23d997;
+    color: white;
+  }
+}
+h2{
+  font-weight: lighter;
+  font-size: 4rem;
+}
+h3{
+  color: white;
+}
+h4{
+  font-weight: bold;
+  color: #23d997;
+}
+a{
+  font-size: 1.1rem;
+}
+span{
+  font-weight: bold;
+  color: #23d997;
+}
+p{
+  padding: 3rem 0rem;
+  color: #ccc;
+  font-size: 1.4rem;
+  line-height: 150%;
+}
+
+`;
+export default GlobalStyle;
+// ed: u can also nest things with styled components...
+// so as u can see u can add an and:hover if u wanna add
+// a hover or if you wanna add and:focus
+
+// what is nest
