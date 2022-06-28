c81d4e85695ad61edc2b407af4ccdd45ef70ca13
htehrani98 <h.tehrani1377@gmail.com>
2022-06-28 19:41:35 +0430

    9.react router issue

--- src/App.js
+++ src/App.js
@@ -4,13 +4,27 @@ import GlobalStyle from "./components/GlobalStyle";
 //Import Pages
 import AboutUs from "./pages/AboutUs";
 import Nav from "./components/Nav";
+import ContactUS from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+// Router
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <Nav></Nav>
-      <AboutUs />
+      <Nav />
+      <Switch>
+        <Route path="/">
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUS />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/ContactUs.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ContactUS = () => {
+  return (
+    <div>
+      <h1>Contact US</h1>
+    </div>
+  );
+};
+
+export default ContactUS;
