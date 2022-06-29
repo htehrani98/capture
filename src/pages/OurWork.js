494d621a9237a486be453fa753ce6a31a1453aaa
htehrani98 <h.tehrani1377@gmail.com>
2022-06-29 19:20:10 +0430

    10. Problem

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -1,11 +1,44 @@
 import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+//Image
+import athlete from "../img/athlete-small.png";
+import theracer from "../img/theracer-small.png";
+import goodtimes from "../img/goodtimes-small.png";
 
 const OurWork = () => {
   return (
-    <div>
-      <h1>Our Work</h1>
-    </div>
+    <Work>
+      <Movie>
+        <h2>The Athlete</h2>
+        <div className="line"></div>
+        <Link>
+          <img src={athlete} alt="Athlete" />
+        </Link>
+      </Movie>
+      <Movie>
+        <h2>The Racer</h2>
+        <div className="line"></div>
+        <Link>
+          <img src={theracer} alt="theracer" />
+        </Link>
+      </Movie>
+      <Movie>
+        <h2>Good Times</h2>
+        <div className="line"></div>
+        <Link>
+          <img src={goodtimes} alt="Athlete" />
+        </Link>
+      </Movie>
+    </Work>
   );
 };
+
+const Work = styled.div`
+  min-height: 100vh;
+`;
+const Movie = styled.div`
+  padding-bottom: 10rem;
+`;
 
 export default OurWork;
