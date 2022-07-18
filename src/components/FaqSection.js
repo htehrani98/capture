9b98fbaaf8e2bb95073171acb9ae13f9e5aeb80e
htehrani98 <h.tehrani1377@gmail.com>
2022-07-18 15:22:14 +0430

    17.issue

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,54 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { About } from "../styles";
 
 const FaqSection = () => {
+  const [faqToggle, setFaqToggle] = useState([false, false, false, false]);
   return (
     <Faq>
       <h2>
         Any Questions <span>FAQ</span>
       </h2>
-      <div className="question">
+      <div onClick={() => setFaqToggle(!faqToggle[0])} className="question">
         <h4>How Do I Start?</h4>
         <div className="answer">
           <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem,
-            molestiae!
-          </p>
+          {faqToggle[0] && (
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem,
+              molestiae!
+            </p>
+          )}
         </div>
         <div className="faq-line"></div>
       </div>
-      <div className="question">
+      <div onClick={() => setFaqToggle(!faqToggle[1])} className="question">
         <h4>How Do I Start?</h4>
         <div className="answer">
           <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem,
-            molestiae!
-          </p>
+          {faqToggle[1] && (
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem,
+              molestiae!
+            </p>
+          )}
         </div>
         <div className="faq-line"></div>
       </div>
-      <div className="question">
+      <div onClick={() => setFaqToggle(!faqToggle[2])} className="question">
         <h4>Different Payment Methods</h4>
         <div className="answer">
           <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem,
-            molestiae!
-          </p>
+          {faqToggle[2] && (
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem,
+              molestiae!
+            </p>
+          )}
         </div>
         <div className="faq-line"></div>
       </div>
-      <div className="question">
+      <div onClick={() => setFaqToggle(!faqToggle[3])} className="question">
         <h4>What Product Do You Offer?</h4>
         <div className="answer">
           <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem,
-            molestiae!
-          </p>
+          {faqToggle[3] && (
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem,
+              molestiae!
+            </p>
+          )}
         </div>
         <div className="faq-line"></div>
       </div>
